9be1b902a46cdbfdf04efdcb803f07c59b485a56
tronglt <trongle1711@gmail.com>
2023-10-22 23:32:06 +0700

    Add tailwind and maud.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 use axum::{routing::get, Router};
+use maud::{html, DOCTYPE};
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route(
-        "/",
-        get(|| async {
-            return "Hello, World!";
-        }),
-    );
+    let app = Router::new()
+        .nest_service("/public", ServeDir::new("public"))
+        .route(
+            "/",
+            get(|| async {
+                return html! {
+                    (DOCTYPE)
+                    head {
+                        link rel="stylesheet" href="/public/css/app.css";
+                    }
+                    h1 class="underline text-lg" { "Maud + Axum + Tailwindcss + HTMx" }
+                };
+            }),
+        );
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
