84a6f6685f7f642fd369b752ddea433ac0b1bff8
tronglt <trongle1711@gmail.com>
2023-10-23 11:58:33 +0700

    Add register page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,27 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("daisyui"),
+  ],
+  daisyui: {
+    themes: [
+      "dark",
+      {
+        light: {
+          "primary": "#000000",
+          "secondary": "#cfa4f9",
+          "accent": "#7bce4e",
+          "neutral": "#22292f",
+          "base-100": "#ffffff",
+          "info": "#a5d4e3",
+          "success": "#1ee6cb",
+          "warning": "#f4ac48",
+          "error": "#fa1946",
+        }
+      }
+    ]
+  }
 }
 
